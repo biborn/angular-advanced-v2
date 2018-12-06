0bb66106e0d8039a91342f28e6bc467065eccb55
Rashid Razak <rashid@bayo.my>
2018-12-07 06:49:53 +0800

    Add callback function
    This callback function will receive callback from template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,18 @@ import { User } from './auth-form/auth-form.interface';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  rememberMe: boolean = false;
 
   createUser(user: User) {
     console.log('Create account', user);
   }
 
   loginUser(user: User) {
-    console.log('Login', user);
+    console.log('Login', user, this.rememberMe);
+  }
+
+  rememberUser(remember: boolean) {
+    console.log('Remember', remember);
+    this.rememberMe = remember;
   }
 }
